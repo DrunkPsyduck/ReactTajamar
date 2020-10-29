bb27de7c568f45270f5fa1df03bcae89a08cbe12
Mario Canales <Mario.Canales@tajamar365.com>
2020-10-29 09:26:04 +0100

    Se introducen cambios en 'PrimeraClase'

--- reactclasesjsx/src/components/primeraClase/PrimeraClase.js
+++ reactclasesjsx/src/components/primeraClase/PrimeraClase.js
@@ -1,16 +1,35 @@
 import React from "react";
 import "./primeraClase.css";
 
-class PrimeraClase extends React.Component {
-    render() {
-        //Evaluado en todo momento
+//! Se puede declarar funciones fuera de la classe pero no se pueden utilizar valores de clase.
+//! Puede recibir valores. Para llamar a las funciones externa no se utiliza 'this'
+function miFuncion() {
+  console.log("esto es miFuncion");
+}
 
-        return (
-            <React.Fragment>
-                <h1 className="rojo">Primer componente class</h1>
-            </React.Fragment >
-        )
-    }
+class PrimeraClase extends React.Component {
+  //Evaluado en todo momento
+  // sin palabras clave(let, cons , function)..
+  contador = 1;
+  metodoAccion = () => {
+    console.log("Contando " + this.contador);
+    this.contador++;
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <h1 className="rojo">Primer componente class</h1>
+        <button onClick={this.metodoAccion}> Contar </button>
+        <button
+          onClick={() => {
+            miFuncion();
+          }}
+        >
+          Pulsar
+        </button>
+      </React.Fragment>
+    );
+  }
 }
 
 export default PrimeraClase;
