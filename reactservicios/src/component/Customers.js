55456e0845e7a375f27c51aa81d470a3fda172c0
Mario Canales <Mario.Canales@tajamar365.com>
2020-11-03 14:15:56 +0100

    Se implementa la promesa con axios.

--- reactservicios/src/component/Customers.js
+++ reactservicios/src/component/Customers.js
@@ -2,7 +2,15 @@ import React, { Component } from "react";
 import axios from "axios";
 
 export default class Customer extends Component {
-  cargarClientes = () => {};
+  urlCustomers = "http://northwind.netcore.io/customers?format=json";
+  state = {
+    customers: [],
+  };
+  cargarClientes = () => {
+    axios.get(this.urlCustomers).then((res) => {
+      console.log(res.data.customers);
+    });
+  };
 
   componentWillMount = () => {
     this.cargarClientes();
