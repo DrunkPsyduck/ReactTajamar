f7433c6ccadb659d6d6744ddea86dc1bec57af46
Mario Canales <Mario.Canales@tajamar365.com>
2020-11-03 14:07:32 +0100

    cambios?

---
+++ reactservicios/src/component/App/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Customer from "./../Customers";
+
+function App() {
+  return (
+    <div className="App">
+      <Customer />
+    </div>
+  );
+}
+
+export default App;
---
+++ reactservicios/src/component/Customers.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+export default class Customer extends Component {
+  cargarClientes = () => {};
+
+  componentWillMount = () => {
+    this.cargarClientes();
+  };
+  render() {
+    return <div></div>;
+  }
+}
