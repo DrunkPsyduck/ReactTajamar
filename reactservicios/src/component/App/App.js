358a758d6ae887989ac1d59cd1aa46f854299001
Mario Canales <Mario.Canales@tajamar365.com>
2020-11-05 11:45:05 +0100

    Cambio en la importacion de los JS de Bootstrap

--- reactservicios/src/component/App/App.js
+++ reactservicios/src/component/App/App.js
@@ -8,7 +8,7 @@ import Router from "./../Router";
 import "bootstrap/dist/css/bootstrap.min.css";
 import $ from "jquery";
 import Popper from "popper.js";
-import "bootstrap/dist/js/bootstrap.bundle";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import Departamentos from "./../Crud/Departamentos";
 function App() {
   return (
