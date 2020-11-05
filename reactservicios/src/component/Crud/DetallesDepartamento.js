b172ba730a588d9a7c173b03fa0d7dd11fd700cf
Mario Canales <Mario.Canales@tajamar365.com>
2020-11-05 13:43:14 +0100

    Se crea 'DetallesDepartamento.js'

---
+++ reactservicios/src/component/Crud/DetallesDepartamento.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import axios from "axios";
+import Global from "./../../Global";
+
+export default class DetallesDepartamento extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <div></div>;
+  }
+}
