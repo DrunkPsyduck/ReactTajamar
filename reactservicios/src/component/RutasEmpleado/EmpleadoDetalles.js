475412ae86446895cdd9cc8c24b058fb539a1880
Mario Canales <Mario.Canales@tajamar365.com>
2020-11-05 10:57:00 +0100

    Se añade la clase 'resultado' en 'Estilos.css'. Se añade la lógica faltante en 'EmpleadoDetalles.js'

--- reactservicios/src/component/RutasEmpleado/EmpleadoDetalles.js
+++ reactservicios/src/component/RutasEmpleado/EmpleadoDetalles.js
@@ -1,14 +1,42 @@
 import React, { Component } from "react";
-
+import Global from "./../../Global";
+import axios from "axios";
 export default class EmpleadoDetalles extends Component {
   //Se reciben props con un constructor
   constructor(props) {
     super(props);
+    console.log("empleado" + this.props.idEmpleado);
   }
+
+  state = {
+    empleado: {},
+    status: false,
+  };
+  mostrarEmpleado = () => {
+    let request = "/api/empleados/" + this.props.idEmpleado;
+    axios.get(Global.empleados + request).then((res) => {
+      this.setState({
+        empleado: res.data,
+        status: true,
+      });
+    });
+    console.log(this.state);
+  };
+
+  componentDidMount = () => {
+    this.mostrarEmpleado();
+  };
   render() {
     return (
       <div>
-        <h1>Hello world , {this.props.idEmpleado}</h1>
+        {this.state.status && (
+          <div>
+            <h2 className="resultado">
+              Apellido: {this.state.empleado.apellido}
+            </h2>
+            <h2 className="resultado">Oficio: {this.state.empleado.oficio}</h2>
+          </div>
+        )}
       </div>
     );
   }
